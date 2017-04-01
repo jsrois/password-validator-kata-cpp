51cebc7b569f00b082fa6fd905f371e9c583931e
jsrois <javier.sanchez.rois@gmail.com>
2017-04-01 16:25:54 +0200

    3P - Passing test: uppercase

--- src/Password.cpp
+++ src/Password.cpp
@@ -3,13 +3,15 @@
 //
 
 #include "Password.h"
+using namespace std;
 
 Password::Password(const std::string& password) {
     this->password = password;
 }
 
 bool Password::isValid() const {
-    return hasMinimumLength();
+    return hasMinimumLength() &&
+            any_of(begin(password),end(password),::isupper);
 }
 
 bool Password::hasMinimumLength() const { return (password.length() >= 8); }
