47dc2b2b81275848f61d1a25de33eae744b1a794
jsrois <javier.sanchez.rois@gmail.com>
2017-04-01 16:14:17 +0200

    1 - Failing (not compiling) first test

---
+++ test/password.test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Javi on 1/4/17.
+//
+
+
+#include <catch/catch.hpp>
+
+TEST_CASE("Password","[password]") {
+    SECTION("is valid if it fulfills all the requirements") {
+        REQUIRE(Password("ABcd123_").isValid());
+    }
+}
